b6b2bbafe7528c4adc04291bafee091394d18e10
Ed <eduardo_casillas@hotmail.com>
2021-04-26 17:44:22 -0700

    Included enumerator on StatisticSection.

--- Runtime/StatisticSection.cs
+++ Runtime/StatisticSection.cs
@@ -1,12 +1,31 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace GlobalstatsIO {
 	[Serializable]
-	public class StatisticSection {
+	public class StatisticSection : IEnumerable<LeaderboardValue> {
 		public RanksData better_ranks;
 		public LeaderboardValue user_rank;
 		public RanksData worse_ranks;
+
+		public IEnumerator<LeaderboardValue> GetEnumerator() {
+			if (better_ranks?.data != null) {
+				foreach (var entry in better_ranks.data) {
+					yield return entry;
+				}
+			}
+
+			if (user_rank != null) yield return user_rank;
+
+			if (worse_ranks?.data != null) {
+				foreach (var entry in worse_ranks.data) {
+					yield return entry;
+				}
+			}
+		}
+
+		IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 	}
 
 	[Serializable]
